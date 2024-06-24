81db4b3f7d8e6ac0204c232ca0a9a1af8fc63cee
MetroKitten <113661103+MetroKitten@users.noreply.github.com>
2024-06-25 00:21:33 +1000

    Initial Commit

---
+++ src/textnode.py
@@ -0,0 +1,11 @@
+Class TextNode:
+    def __init__(self, text, text_type, url):
+    self.text = text
+    self.text_type = text_type
+    self.url = url
+
+    def __eq__(self):
+    
+    def __repr__(self):
+        return f"{"
+
