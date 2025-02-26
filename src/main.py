906bda6103141c0f12c2398242f50cd4c18fd718
shaneplunkett <113661103+shaneplunkett@users.noreply.github.com>
2025-02-26 17:55:31 +1100

    Update

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
-from generator import *
 import os
+
+from generator import *
 
 dir_path_static = "./static"
 dir_path_public = "./public"
 dir_path_content = "./content"
 template_path = "./template.html"
+
 
 def main():
     print("Deleting public directory...")
